49e4e16f7ad57ac6662faf98e5a7a982493b5bc9
gokilasoundarrajan <gokilasoundarrajan@gmail.com>
2021-03-14 10:02:45 +0530

    Hello World

---
+++ src/main/java/com/lets/Lets.java
@@ -0,0 +1,13 @@
+package com.lets;
+
+/**
+ * Hello world!
+ *
+ */
+public class Lets 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
